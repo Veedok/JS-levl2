2c9e906109e7a3d3fac754c634d568f5d304deb4
Anatoly <Konakov88@gmail.com>
2021-03-12 11:45:36 +0300

    Правки по замечаниям преподователя

--- project/js/vu.js
+++ project/js/vu.js
@@ -1,3 +1,5 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 const myVeu = new Vue({
     el: '#myVue',
     data: {
@@ -7,6 +9,9 @@ const myVeu = new Vue({
         serchProduct: [],
         cartUrl: '/getBasket.json',
         catalogUrl: '/catalogData.json',
+        cartItems: [],
+        filtered: [],
+        imgCatalog: 'https://placehold.it/200x150',
     },
     methods: {
         getJson(url) {
@@ -15,26 +20,121 @@ const myVeu = new Vue({
                 .catch(error => {
                     console.log(error);
                 })
-        },
-        mounted() {
-            this.getJson(`${API + this.cartUrl}`)
-                .then(data => {
-                    for (let el of data.contents) {
-                        this.cartItems.push(el);
-                    }
-                });
-            this.getJson(`${API + this.catalogUrl}`)
-                .then(data => {
-                    for (let el of data) {
-                        this.products.push(el);
-                        this.filtered.push(el);
-                    }
-                });
-        },
+        },        
         test() {
             let regexp = new RegExp(this.inpserch, 'i');
             this.serchProduct = this.products.filter(el => regexp.test(el.product_name));
         }
-    }
+    },
+    mounted() {
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let el of data.contents) {
+                    this.cartItems.push(el);
+                }
+            });
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+            console.log(products);
+    },
+    template: `
+    <div class="products">
+        <product v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+    </div>
+`
 
+});
+Vue.component('product', {
+    props: ['product', 'img'],
+    template: `
+    <div class="product-item">
+                <img :src="img" alt="Some img">
+                <div class="desc">
+                    <h3>{{product.product_name}}</h3>
+                    <p>{{product.price}}₽</p>
+                   <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>
+<!-- 2                    <button class="buy-btn" @click="$parent.$parent.$refs.cart.addProduct(product)">Купить</button>-->
+                </div>
+            </div>
+    `
+});
+Vue.component('cart', {
+    data(){
+      return {
+          imgCart: 'https://placehold.it/50x100',
+          cartUrl: '/getBasket.json',
+          cartItems: [],
+          showCart: false,
+      }
+    },
+    methods: {
+        addProduct(product){
+            this.$parent.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if(data.result === 1){
+                        let find = this.cartItems.find(el => el.id_product === product.id_product);
+                        if(find){
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({quantity: 1}, product);
+                            this.cartItems.push(prod)
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
+        },
+        remove(item) {
+            this.$parent.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if(data.result === 1) {
+                        if(item.quantity>1){
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1)
+                        }
+                    }
+                })
+        },
+    },
+   
+    template: `
+        <div>
+            <button class="btn-cart" type="button" ><i class="fas fa-shopping-cart svgCart"></i></button>
+            <div class="cart-block" >
+                <p v-if="!cartItems.length">Корзина пуста</p>
+                <cart-item class="cart-item" 
+                v-for="item of cartItems" 
+                :key="item.id_product"
+                :cart-item="item" 
+                :img="imgCart"
+                @remove="remove">
+                </cart-item>
+            </div>
+        </div>`
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem', 'img'],
+    template: `
+                <div class="cart-item">
+                    <div class="product-bio">
+                        <img :src="img" alt="Some image">
+                        <div class="product-desc">
+                            <p class="product-title">{{cartItem.product_name}}</p>
+                            <p class="product-quantity">Количество: {{cartItem.quantity}}</p>
+                            <p class="product-single-price">{{cartItem.price}}₽ за единицу</p>
+                        </div>
+                    </div>
+                    <div class="right-block">
+                        <p class="product-price">{{cartItem.quantity*cartItem.price}}₽</p>
+                        <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                    </div>
+                </div>
+    `
 });
